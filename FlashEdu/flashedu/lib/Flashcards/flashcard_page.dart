8525c4392454544ba2b30b20cf5124888ea7ed1d
Lariterrinha <lalaterrinha14@gmail.com>
2025-05-16 17:29:10 -0300

    Edit flash cardpage

--- FlashEdu/flashedu/lib/Flashcards/flashcard_page.dart
+++ FlashEdu/flashedu/lib/Flashcards/flashcard_page.dart
@@ -6,59 +6,124 @@ import '../models/folder.dart';
 import '../models/flashcard.dart';
 import 'db_helper.dart';
 
-class FlashcardPage extends StatelessWidget {
+class FlashcardPage extends StatefulWidget {
   final Folder folder;
   FlashcardPage({required this.folder});
 
   @override
+  _FlashcardPageState createState() => _FlashcardPageState();
+}
+
+class _FlashcardPageState extends State<FlashcardPage> {
+
+  SwipeableCardSectionController controller = SwipeableCardSectionController();
+  List<FlashCard> itens = [];
+  int indexFlashcards = 0;
+
+  FlashCard buildFlashCard(Flashcard fc, Size screen) {
+    return FlashCard(
+      width: screen.width * 0.9,
+      height: screen.height * 0.5,
+      frontWidget: () => Container(
+        decoration: BoxDecoration(
+          color: UserPreferencesService.getThemeColor(),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Center(
+          child: Text(fc.frontText,
+            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+        ),
+      ),
+      backWidget: () => Container(
+        decoration: BoxDecoration(
+          color: Color.lerp(UserPreferencesService.getThemeColor(), Colors.white, 0.3),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Center(
+          child: Text(fc.backText,
+            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+
+
+  @override
   Widget build(BuildContext context) {
-    SwipeableCardSectionController controller = SwipeableCardSectionController();
     Size screen = MediaQuery.of(context).size;
 
     return Scaffold(
       appBar: AppBar(
-          title: Text('Folder: ${folder.name}', style: TextStyle(color: Colors.white)),
+          title: Text('Folder: ${widget.folder.name}', style: TextStyle(color: Colors.white)),
           backgroundColor: UserPreferencesService.getThemeColor(),
           iconTheme: IconThemeData(color: Colors.white),
       ),
 
       body: FutureBuilder<List<Flashcard>>(
-        future: DatabaseHelper().getFlashcardsByFolder(folder.id!),
+        future: DatabaseHelper().getFlashcardsByFolder(widget.folder.id!),
         builder: (context, snapshot) {
           if (!snapshot.hasData) return Center(child: CircularProgressIndicator());
           if (snapshot.data!.isEmpty) return Center(child: Text('No flashcard to show.'));
 
-          return SwipeableCardsSection(
-            cardController: controller,
-            context: context,
-            items: snapshot.data!.map((fc) => FlashCard(
-              width: screen.width * 0.9,
-              height: screen.height * 0.5,
-              frontWidget: () => Container(
-                decoration: BoxDecoration(
-                  color: UserPreferencesService.getThemeColor(),
-                  borderRadius: BorderRadius.circular(16),
+          if (itens.isEmpty) {
+
+            itens = snapshot.data!.map((fc) {
+              final front = fc.frontText;
+              final back = fc.backText;
+
+              return FlashCard(
+                width: screen.width * 0.9,
+                height: screen.height * 0.5,
+                frontWidget: () => Container(
+                  decoration: BoxDecoration(
+                    color: UserPreferencesService.getThemeColor(),
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  child: Center(
+                    child: Text(front, style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    )),
+                  ),
                 ),
-                child: Center(
-                  child: Text(fc.frontText, style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: Colors.white)),
+                backWidget: () => Container(
+                  decoration: BoxDecoration(
+                    color: Color.lerp(UserPreferencesService.getThemeColor(), Colors.white, 0.3),
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  child: Center(
+                    child: Text(back, style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    )),
+                  ),
                 ),
-              ),
-              backWidget: () => Container(
-                decoration: BoxDecoration(
-                  color: Color.lerp(UserPreferencesService.getThemeColor(), Colors.white, 0.3) ,
-                  borderRadius: BorderRadius.circular(16),
-                ),
-                child: Center(
-                  child: Text(fc.backText, style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: Colors.white)),
-                ),
-              ),
+              );
+            }).toList();
 
 
-            )).toList(),
-            enableSwipeUp: false,
-            enableSwipeDown: false,
-            onCardSwiped: (dir, index, widget) {},
-          );
+
+            indexFlashcards = itens.length >= 3 ? 3 : itens.length;
+          }
+
+          return  SwipeableCardsSection(
+                cardController: controller,
+                context: context,
+                items: itens.length >= 3 ? itens.sublist(0, 3) : itens,
+                enableSwipeUp: true,
+                enableSwipeDown: true,
+                onCardSwiped: (dir, index, widget) {
+                  if(indexFlashcards < itens.length){
+                    controller.addItem(itens[indexFlashcards]);
+                    indexFlashcards++;
+                  }
+                  setState(() {});
+                },
+            );
         },
       ),
 
@@ -71,11 +136,11 @@ class FlashcardPage extends StatelessWidget {
           await Navigator.push(
             context,
             MaterialPageRoute(
-              builder: (_) => CreateFlashcardPage(folder: folder),
+              builder: (_) => CreateFlashcardPage(folder: widget.folder),
             ),
           );
-          // rebuild to update the flashcards
-          (context as Element).markNeedsBuild();
+
+          setState(() {}); // Rebuild the screen after adding
         },
       ),
 
